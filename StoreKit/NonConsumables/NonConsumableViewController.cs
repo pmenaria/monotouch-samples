8a17096ce941ad5f1ff3c5b7893e5d2f06b7bff1
Paola Villarreal <paw@paw.mx>
2014-08-06 20:21:43 -0500

    StoreKit/NonConsumables sample ported to 64-bits

--- StoreKit/NonConsumables/NonConsumableViewController.cs
+++ StoreKit/NonConsumables/NonConsumableViewController.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using MonoTouch.StoreKit;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using StoreKit;
+using Foundation;
+using UIKit;
 using System.Drawing;
 
 namespace NonConsumables {
